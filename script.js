f0b0b671cbb03f419d6d39f070f286f59049453e
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-27 12:16:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
+let infiList = document.getElementById("infi-list");
 
+functin addListItem(count){
+	for(let i = 0; i < count; i++){
+		let list = document.createElement("li");
+		list.textContent = `Item ${infiList.children.length + 1}`;
+		infiList.appendChild(list);
+	}
+}
+
+addListItem(10);
+
+infiList.addEventListener("scroll", () => {
+	if(infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight){
+		addListItem(2);
+	}
+})
